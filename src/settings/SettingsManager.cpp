7871cde76c35cd8c437eac7ea534262b4db630eb
mirceabc <blaga_mircea@yahoo.com>
2024-04-17 22:15:00 +0300

    Load , edit and save settings.ini Now u can edit settings.ini file to modify game Display settings. Deleting settings.ini will restore to default settings. Edited values from settings.ini will be saved and applied on next launch. Invalid values will be set with default value.

--- src/settings/SettingsManager.cpp
+++ src/settings/SettingsManager.cpp
@@ -3,9 +3,54 @@
 // Constructor
 SettingsManager::SettingsManager()
 {
-    getDefaultMonitorsWithResolution();
-    LoadSettingsFile("settings.ini");
+    getAllMonitorsWithResolutions();
+    LoadSettingsFromFile("settings.ini");
 }
 
 // Destructor
-SettingsManager::~SettingsManager(){}
+SettingsManager::~SettingsManager() {
+
+}
+
+// Get Default Monitor Resolution
+SettingsManager::ScreenResolution SettingsManager::getDefaultDisplayResolution()
+{
+    int index = monitors[currentSelectedMonitor].resolutions.size() - 1;
+    return monitors[currentSelectedMonitor].resolutions[index];
+}
+
+// GET All Monitors with Resolutions
+std::vector<SettingsManager::Monitor> SettingsManager::getAllMonitorsWithResolutions()
+{
+    int count;
+    GLFWmonitor **display = glfwGetMonitors(&count);
+
+    for (int i = 0; i < count; i++)
+    {
+        Monitor monitor;
+        monitor.monitor_name = glfwGetMonitorName(display[i]);
+
+        int modeCount;
+        const GLFWvidmode *modes = glfwGetVideoModes(display[i], &modeCount);
+        for (int j = 0; j < modeCount; j++)
+        {
+            if ((modes[j].refreshRate == 144) || (modes[j].refreshRate == 120) || (modes[j].refreshRate == 60))
+            {
+                monitor.resolutions.push_back({modes[j].width, modes[j].height, modes[j].refreshRate});
+            }
+        }
+        monitors.push_back(monitor);
+    }
+
+    // DEBUG - Print monitors
+    // for(int i = 0; i < monitors.size(); i++)
+    // {
+    //     std::cout << "Monitor: " << monitors[i].monitor_name << std::endl;
+    //     for(int j = 0; j < monitors[i].resolutions.size(); j++)
+    //     {
+    //         std::cout << "Resolution: " << monitors[i].resolutions[j].width << "x" << monitors[i].resolutions[j].height << " @ " << monitors[i].resolutions[j].hz << "Hz" << std::endl;
+    //     }
+    // }
+
+    return monitors;
+}
--- src/settings/SettingsManager.h
+++ src/settings/SettingsManager.h
@@ -13,14 +13,14 @@
 class SettingsManager
 {
 private:
-    // Window Settings
-    int width = 800;
-    int height = 600;
+    // Default Window Settings
+    GLFWmonitor *monitor = glfwGetPrimaryMonitor();
+    int width = glfwGetVideoMode(monitor)->width;
+    int height = glfwGetVideoMode(monitor)->height;
     const char *title = "Titanium Chess";
-    GLFWmonitor *monitor = nullptr;
-    int refreshRate = 60;
+    int refreshRate = glfwGetVideoMode(monitor)->refreshRate;
 
-    // Resolution + HZ
+    // Resolution and Refresh Rate
     struct ScreenResolution
     {
         int width;
@@ -39,7 +39,7 @@ private:
     std::vector<Monitor> monitors;
 
     // Fullscreen
-    bool fullscreen = 0;
+    bool fullscreen = 1;
 
     // VSync
     bool vSync = 1;
@@ -48,37 +48,9 @@ private:
     int currentSelectedMonitor = 0;
     // ScreenResolution currentSelectedResolution = {0, 0, 0};
 
-    // Save Default Settings File
-    void SaveDefaultSettingsFile(const std::string &filename)
+    // Save Default Settings to file settings.ini
+    void SaveDefaultSettingsToFile(const std::string &filename, const std::string &filePath)
     {
-        // Check if the directory exists
-        std::filesystem::path documentsDir = std::filesystem::path(getenv("HOME")) / "Documents";
-        if (!std::filesystem::exists(documentsDir))
-        {
-            std::cerr << "Directorul \"Documente\" nu există!" << std::endl;
-            return;
-        }
-
-        // Check if the directory exists
-        std::filesystem::path gameDir = documentsDir / "My Games" / "TitaniumChess";
-        if (!std::filesystem::exists(gameDir))
-        {
-            std::filesystem::create_directories(gameDir);
-        }
-
-        // Check if the file already exists
-        // TODO: Check if the file integrity is correct
-        std::filesystem::path filePath = gameDir / filename;
-        if (std::filesystem::exists(filePath))
-        {
-            std::cerr << "Fișierul " << filePath << " există deja. Nu se poate crea din nou." << std::endl;
-            return;
-        }
-        else
-        {
-            // TODO Chech file integrity
-        }
-
         // Write the default settings to the file
         std::ofstream file(filePath);
         if (file.is_open())
@@ -101,53 +73,86 @@ private:
         }
     }
 
-    // Load Settings File
-    void LoadSettingsFile(const std::string &filename)
+    // Load Settings from file settings.ini
+    void LoadSettingsFromFile(const std::string &filename)
     {
-        // Check if the directory exists
-        std::filesystem::path documentsDir = std::filesystem::path(getenv("HOME")) / "Documents";
-        if (!std::filesystem::exists(documentsDir))
-        {
-            std::cerr << "Directorul \"Documente\" nu există!" << std::endl;
-            return;
-        }
-
-        // Check if the directory exists
-        std::filesystem::path gameDir = documentsDir / "My Games" / "TitaniumChess";
+        // Check if the game settings directory exists
+        std::filesystem::path gameDir = std::filesystem::path(getenv("HOME")) / "Documents" / "My Games" / "TitaniumChess";
+        std::filesystem::path filePath = gameDir / filename;
         if (!std::filesystem::exists(gameDir))
         {
-            SaveDefaultSettingsFile("settings.ini");
+            std::filesystem::create_directories(gameDir);
+            SaveDefaultSettingsToFile(filename, filePath);
             return;
         }
 
-        // Check if the file already exists
-        std::filesystem::path filePath = gameDir / filename;
+        // Check if the settings.ini file already exists
         if (std::filesystem::exists(filePath))
         {
-           if (CheckSettingsFile("settings.ini") == true)
-           {
-                // Load the settings
-                mINI::INIFile file(filePath);
-                mINI::INIStructure ini;
-                file.read(ini);
+            mINI::INIFile file(filePath);
+            mINI::INIStructure ini;
+            file.read(ini);
 
-                setWidth(std::stoi(ini["Display"]["resolutionWidth"]));
-                setHeight(std::stoi(ini["Display"]["resolutionHeight"]));
-                setRefreshRate(std::stoi(ini["Display"]["refreshRate"]));
-                setFullscreen(std::stoi(ini["Display"]["fullscreen"]));
-                setVSync(std::stoi(ini["Display"]["vSync"]));
-           }
+            CheckSettingsFromFile(file, ini);
+
+            setWidth(std::stoi(ini["Display"]["resolutionWidth"]));
+            setHeight(std::stoi(ini["Display"]["resolutionHeight"]));
+            setRefreshRate(std::stoi(ini["Display"]["refreshRate"]));
+            if ((std::stoi(ini["Display"]["fullscreen"])) == 1)
+            {
+                setMonitor(glfwGetPrimaryMonitor());
+                setFullscreen(true);
+            }
+            else if ((std::stoi(ini["Display"]["fullscreen"])) == 0)
+            {
+                setMonitor(nullptr);
+                setFullscreen(false);
+            }
+            setVSync(std::stoi(ini["Display"]["vSync"]));
         }
         else
         {
-            SaveDefaultSettingsFile("settings.ini");
-            return;
+            SaveDefaultSettingsToFile(filename, filePath);
         }
     }
 
-    bool CheckSettingsFile(const std::string &filename)
+    // Check if the settings.ini file is valid and update it if necessary
+    void CheckSettingsFromFile(mINI::INIFile &file, mINI::INIStructure &ini)
     {
-        return true;
+        // TODO UPDATE: Check if is monitor supported resolutions in file
+        if ((ini["Display"]["resolutionWidth"].empty()) || (std::stoi(ini["Display"]["resolutionWidth"]) < 800) || (std::stoi(ini["Display"]["resolutionWidth"]) > width))
+        {
+            ini["Display"]["resolutionWidth"] = std::to_string(width);
+            file.write(ini);
+        }
+
+        if ((ini["Display"]["resolutionHeight"].empty()) || (std::stoi(ini["Display"]["resolutionHeight"]) < 600) || (std::stoi(ini["Display"]["resolutionHeight"]) > height))
+        {
+            ini["Display"]["resolutionHeight"] = std::to_string(height);
+            file.write(ini);
+        }
+
+        if (ini["Display"]["refreshRate"].empty() || ini["Display"]["refreshRate"] != "60" || ini["Display"]["refreshRate"] != "120" || ini["Display"]["refreshRate"] != "144")
+        {
+            ini["Display"]["refreshRate"] = std::to_string(refreshRate);
+            file.write(ini);
+        }
+
+        if ((ini["Display"]["fullscreen"].empty()) ||
+            (ini["Display"]["fullscreen"] != "0" && ini["Display"]["fullscreen"] != "1"))
+        {
+
+            ini["Display"]["fullscreen"] = getFullscreen() ? "1" : "0";
+            file.write(ini);
+        }
+
+        if ((ini["Display"]["vSync"].empty()) ||
+            (ini["Display"]["vSync"] != "0" && ini["Display"]["vSync"] != "1"))
+        {
+
+            ini["Display"]["vSync"] = getVSync() ? "1" : "0";
+            file.write(ini);
+        }
     }
 
 public:
@@ -157,79 +162,39 @@ public:
     // Destructor
     ~SettingsManager();
 
-    // GET Width
-    int getWidth(){return width;}
-    // GET Height
-    int getHeight(){return height;}
-    // GET Refresh Rate
-    int getRefreshRate(){return refreshRate;}
-    // GET Fullscreen
-    bool getFullscreen(){return fullscreen;}
-    // GET VSync
-    bool getVSync(){return vSync;}
-
-
-    // GET Monitor
-    GLFWmonitor *getMonitor(){return monitor;}
-
-    // GET Title
-    const char *getTitle(){return title;}
-
-
     // SET Width
-    void setWidth(int w){width = w;}
+    void setWidth(int w) { width = w; }
     // SET Height
-    void setHeight(int h){height = h;}
+    void setHeight(int h) { height = h; }
     // SET Refresh Rate
-    void setRefreshRate(int hz){refreshRate = hz;}
+    void setRefreshRate(int hz) { refreshRate = hz; }
     // SET Fullscreen
-    void setFullscreen(bool fs){fullscreen = fs;}
+    void setFullscreen(bool fs) { fullscreen = fs; }
     // SET VSync
-    void setVSync(bool vs){vSync = vs;}
+    void setVSync(bool vs) { vSync = vs; }
     // SET Monitor
-    void setMonitor(GLFWmonitor *m){monitor = m;}
+    void setMonitor(GLFWmonitor *m) { monitor = m; }
 
-    // GET Default Monitors with Resolutions
-    std::vector<Monitor> getDefaultMonitorsWithResolution()
-    {
-        int count;
-        GLFWmonitor **display = glfwGetMonitors(&count);
+    // GET Width
+    int getWidth() { return width; }
+    // GET Height
+    int getHeight() { return height; }
+    // GET Refresh Rate
+    int getRefreshRate() { return refreshRate; }
+    // GET Fullscreen
+    bool getFullscreen() { return fullscreen; }
+    // GET VSync
+    bool getVSync() { return vSync; }
+    // GET Monitor
+    GLFWmonitor *getMonitor() { return monitor; }
+    // GET Title
+    const char *getTitle() { return title; }
 
-        for (int i = 0; i < count; i++)
-        {
-            Monitor monitor;
-            monitor.monitor_name = glfwGetMonitorName(display[i]);
+    // GET All Monitors with Resolutions
+    std::vector<Monitor> getAllMonitorsWithResolutions();
 
-            int modeCount;
-            const GLFWvidmode *modes = glfwGetVideoModes(display[i], &modeCount);
-            for (int j = 0; j < modeCount; j++)
-            {
-                if ((modes[j].refreshRate == 144) || (modes[j].refreshRate == 120) || (modes[j].refreshRate == 60))
-                {
-                    monitor.resolutions.push_back({modes[j].width, modes[j].height, modes[j].refreshRate});
-                }
-            }
-            monitors.push_back(monitor);
-        }
-
-        // DEBUG - Print monitors
-        // for(int i = 0; i < monitors.size(); i++)
-        // {
-        //     std::cout << "Monitor: " << monitors[i].monitor_name << std::endl;
-        //     for(int j = 0; j < monitors[i].resolutions.size(); j++)
-        //     {
-        //         std::cout << "Resolution: " << monitors[i].resolutions[j].width << "x" << monitors[i].resolutions[j].height << " @ " << monitors[i].resolutions[j].hz << "Hz" << std::endl;
-        //     }
-        // }
-
-        return monitors;
-    };
-
-    ScreenResolution getDefaultDisplayResolution()
-    {
-        int index = monitors[currentSelectedMonitor].resolutions.size() - 1;
-        return monitors[currentSelectedMonitor].resolutions[index];
-    }
+    // GET Default Monitor Resolution
+    ScreenResolution getDefaultDisplayResolution();
 };
 
 #endif // SETTINGSMANAGER_H
